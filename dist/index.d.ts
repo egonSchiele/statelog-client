128da933a0404ae53adaeef40f3ec4be1a85af80
Aditya Bhargava <adit@adit.io>
2026-01-04 19:35:44 -0600

    dist

---
+++ dist/index.d.ts
@@ -0,0 +1,60 @@
+export type JSONEdge = {
+    type: "regular";
+    to: string;
+} | {
+    type: "conditional";
+    adjacentNodes: readonly string[];
+};
+export declare class StatelogClient {
+    private host;
+    private debugMode;
+    private tid;
+    constructor(host: string, debug?: boolean);
+    debug(message: string, data: any): void;
+    graph({ nodes, edges, startNode, }: {
+        nodes: string[];
+        edges: Record<string, JSONEdge>;
+        startNode?: string;
+    }): void;
+    enterNode({ nodeId, data }: {
+        nodeId: string;
+        data: any;
+    }): void;
+    exitNode({ nodeId, data, timeTaken, }: {
+        nodeId: string;
+        data: any;
+        timeTaken?: number;
+    }): void;
+    beforeHook({ nodeId, startData, endData, timeTaken, }: {
+        nodeId: string;
+        startData: any;
+        endData: any;
+        timeTaken?: number;
+    }): void;
+    afterHook({ nodeId, startData, endData, timeTaken, }: {
+        nodeId: string;
+        startData: any;
+        endData: any;
+        timeTaken?: number;
+    }): void;
+    followEdge({ fromNodeId, toNodeId, isConditionalEdge, data, }: {
+        fromNodeId: string;
+        toNodeId: string;
+        isConditionalEdge: boolean;
+        data: any;
+    }): void;
+    promptCompletion({ messages, completion, model, timeTaken, }: {
+        messages: any[];
+        completion: any;
+        model?: string;
+        timeTaken?: number;
+    }): void;
+    toolCall({ toolName, args, output, model, timeTaken, }: {
+        toolName: string;
+        args: any;
+        output: any;
+        model?: string;
+        timeTaken?: number;
+    }): void;
+    post(body: Record<string, any>): void;
+}
---
+++ dist/index.js
@@ -0,0 +1,104 @@
+import { nanoid } from "nanoid";
+export class StatelogClient {
+    constructor(host, debug = false) {
+        this.host = host;
+        this.debugMode = debug;
+        this.tid = nanoid();
+        if (this.debugMode)
+            console.log(`Statelog client initialized with host: ${host} and TID: ${this.tid}`);
+    }
+    debug(message, data) {
+        this.post({
+            type: "debug",
+            message: message,
+            data,
+        });
+    }
+    graph({ nodes, edges, startNode, }) {
+        this.post({
+            type: "graph",
+            nodes,
+            edges,
+            startNode,
+        });
+    }
+    enterNode({ nodeId, data }) {
+        this.post({
+            type: "enterNode",
+            nodeId,
+            data,
+        });
+    }
+    exitNode({ nodeId, data, timeTaken, }) {
+        this.post({
+            type: "exitNode",
+            nodeId,
+            data,
+            timeTaken,
+        });
+    }
+    beforeHook({ nodeId, startData, endData, timeTaken, }) {
+        this.post({
+            type: "beforeHook",
+            nodeId,
+            startData,
+            endData,
+            timeTaken,
+        });
+    }
+    afterHook({ nodeId, startData, endData, timeTaken, }) {
+        this.post({
+            type: "afterHook",
+            nodeId,
+            startData,
+            endData,
+            timeTaken,
+        });
+    }
+    followEdge({ fromNodeId, toNodeId, isConditionalEdge, data, }) {
+        this.post({
+            type: "followEdge",
+            edgeId: `${fromNodeId}->${toNodeId}`,
+            fromNodeId,
+            toNodeId,
+            isConditionalEdge,
+            data,
+        });
+    }
+    promptCompletion({ messages, completion, model, timeTaken, }) {
+        this.post({
+            type: "promptCompletion",
+            messages,
+            completion,
+            model,
+            timeTaken,
+        });
+    }
+    toolCall({ toolName, args, output, model, timeTaken, }) {
+        this.post({
+            type: "toolCall",
+            toolName,
+            args,
+            output,
+            model,
+            timeTaken,
+        });
+    }
+    post(body) {
+        const fullUrl = new URL("/api/logs", this.host);
+        const url = fullUrl.toString();
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                tid: this.tid,
+                data: Object.assign(Object.assign({}, body), { timeStamp: new Date().toISOString() }),
+            }),
+        }).catch((err) => {
+            if (this.debugMode)
+                console.error("Failed to send statelog:", err);
+        });
+    }
+}
